0b3c22d72d892571e71f440b03d7c0606a50950c
Arthur Vu <arthurvu75@gmail.com>
2019-10-10 23:12:26 +0200

    Update dark-trader.rb

--- lib/dark-trader.rb
+++ lib/dark-trader.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 def crypto_names_method
     page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
       crypto_names =[]
-      page.xpath('//*[@id]/td[2]/a').each do |crypto_name|
+      page.xpath('//*[@id]/td[3]').each do |crypto_name|
       crypto_names.push(crypto_name.text)
     end
       puts crypto_names
@@ -14,7 +14,7 @@ end
 def crypto_prices_method
     page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
       crypto_prices =[]
-      page.xpath('//*[@id]/td[5]/a').each do |crypto_price|
+      page.xpath('//*[@id]/td[5]').each do |crypto_price|
       crypto_prices.push(crypto_price.text)
     end
       puts crypto_prices
@@ -22,7 +22,6 @@ end
 
 def perform
     crypto_names_method
-    crypto_prices_method
 end
 
 perform
