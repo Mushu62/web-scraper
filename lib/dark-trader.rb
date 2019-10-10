7c091c6e2c4b5e91aba08ba7192c37e77f8f6b6f
Arthur Vu <arthurvu75@gmail.com>
2019-10-10 13:29:44 +0200

    Update dark-trader.rb

--- lib/dark-trader.rb
+++ lib/dark-trader.rb
@@ -1,1 +1,19 @@
 require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+def crypto_names
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    table = document.at('#currencies-all > tbody')
+    
+    table.search('tr').each do |tr|
+    cells = tr.search('td.text-left.col-symbol, td.no-wrap.market-cap.text-right')
+      
+      end
+
+      cells.each do |cell|
+      text = cell.text.strip
+      
+      puts CSV.generate_line(cells)
+      end
+end
