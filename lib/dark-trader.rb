b75778702153a1d4409d1f96cadca7cb5c1d2996
Arthur Vu <arthurvu75@gmail.com>
2019-10-10 13:47:15 +0200

    Update dark-trader.rb

--- lib/dark-trader.rb
+++ lib/dark-trader.rb
@@ -4,5 +4,5 @@ require 'open-uri'
 
 def crypto_names
     doc = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-    puts page.xpath("/html/body/div[2]/div[2]/div[1]/div[1]/div[3]/div[2]/div/table/tbody/tr[1]/td[3]").text
+    puts page.xpath("/html/body/div[2]/div[2]/div[1]/div[1]/div[3]/div[2]/div/table/tbody/tr[1]/td[3]")
 end
